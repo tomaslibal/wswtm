df91026689247402f16840d199954a9fb0ed1140
Tomas Libal <tomas@libal.eu>
2015-02-15 23:03:37 +0000

    add script that will download the data into the data folder

---
+++ download-data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Not yet fully implemented
+echo "Not yet implemented";
+exit 1;
+
+curl -O http://libal.eu/data/wswtm/image-data.tar.gz
+
+# z - unzip
+# x - extract the file
+# v - verbose
+# f - forcefully done
+tar -zxvf image-data.tar.gz 
