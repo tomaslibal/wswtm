ab20162bdd96acbf21a6ffa210bd4534cf87f228
Tomas Libal <tomas@libal.eu>
2015-02-17 20:10:33 +0000

    add arguments for setting input and output files

--- program/get-pixels.py
+++ program/get-pixels.py
@@ -1,8 +1,27 @@
 from PIL import Image
 import sys
 
-im_path = "../data/class_building/images/152451710_87e0045334_q.jpg"
- 
+im_path = '' 
+out_path = 'out.txt'
+
+# Config
+resample_w = 64
+resample_h = 64
+
+# Value 0 will return only the red component of each pixel value when passed
+# to Image.getdata
+pixel_component = 0
+
+# Set up the variables from the arguments
+if len(sys.argv) >= 2:
+    im_path = sys.argv[1]
+else:
+    print "Usage: get-pixels.py imgPath [outputPath]"
+    sys.exit()
+
+if len(sys.argv) >= 3:
+    out_path = sys.argv[2]
+
 try:
     im = Image.open(im_path)
 except IOError:
@@ -10,7 +29,8 @@ except IOError:
     sys.exit()
 
 
-pixels = list(im.getdata())
+im = im.resize((resample_w, resample_h))
+pixels = list(im.getdata(pixel_component))
 width, height = im.size
 pixelsMN = [pixels[i * width:(i + 1) * width] for i in xrange(height)]
 
