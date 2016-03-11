de311ba1ae3293aeb9b29fb02d11a47dbd194946
Tomas Libal <tomas@libal.eu>
2016-03-11 19:07:30 +0000

    PixelData is an abstraction of a 2 dimensional pixel array

---
+++ src/java/eu/libal/wswtm/PixelData.java
@@ -0,0 +1,19 @@
+package eu.libal.wswtm;
+
+/**
+ *
+ */
+public class PixelData {
+
+    private int[][] pixelData;
+
+    public PixelData() {}
+
+    public int[][] getPixelData() {
+        return pixelData;
+    }
+
+    public void setPixelData(int[][] pixelData) {
+        this.pixelData = pixelData;
+    }
+}
