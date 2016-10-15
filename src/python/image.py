97031f7027e96eeb529d56282b48a4a2f2060eaa
Tomas Libal <tomas@libal.eu>
2016-10-15 14:02:50 +0800

    add python stubs

---
+++ src/python/image.py
@@ -0,0 +1,8 @@
+def load_image(path):
+    pass
+
+def resize(width=32, height=32):
+    pass
+
+def crop_center(width=32, height=32):
+    pass
---
+++ src/python/log.py
@@ -0,0 +1,8 @@
+def log():
+    pass
+
+def debug(msg=""):
+    pass
+
+def error():
+    pass
