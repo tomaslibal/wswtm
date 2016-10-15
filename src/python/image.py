2d95fac6e2e8ca7d967d221dd93d03af927c459a
Tomas Libal <tomas@libal.eu>
2016-10-15 14:07:16 +0800

    add a stub for a function that takes pixels and returns a 2d array of the pixel values

--- src/python/image.py
+++ src/python/image.py
@@ -6,3 +6,9 @@ def resize(width=32, height=32):
 
 def crop_center(width=32, height=32):
     pass
+
+"""
+    Returns a 2d array of 3-tuples where each 3-tuple represents the RGB component of each pixel
+"""
+def image2vec(pixels):
+    pass
