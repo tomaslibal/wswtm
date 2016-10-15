cf2ef12ed25f884124b95bc65c73ac07fc9e58b2
Tomas Libal <tomas@libal.eu>
2016-10-15 14:20:32 +0800

    add a rudimentary test infrastructure (test.sh) together with a failing test

---
+++ test/python/test_image.py
@@ -0,0 +1,9 @@
+import unittest
+
+class ImageTest(unittest.TestCase):
+    def test_should_return_2d_array(self):
+        self.assertEqual(1, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
