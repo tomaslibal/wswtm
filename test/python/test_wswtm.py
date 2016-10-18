227f67732a9fe3742be204bba8c0ed88cb37f6f9
Tomas Libal <tomas@libal.eu>
2016-10-18 23:32:56 +0800

    add test for wswtm class

---
+++ test/python/test_wswtm.py
@@ -0,0 +1,16 @@
+import unittest
+
+from src.python.wswtm import wswtm
+
+w = None
+
+class wswtmTest(unittest.TestCase):
+    def setUp(self):
+        w = wswtm()
+
+    def test_should_return_1d_array(self):
+        self.assertEqual(True, isinstance(w.image2tags('some/image.png')))
+
+
+if __name__ == '__main__':
+    unittest.main()
