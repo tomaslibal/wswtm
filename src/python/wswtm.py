801cab7572f1909eb922d0d744b6b6f808e8fd7c
Tomas Libal <tomas@libal.eu>
2016-10-19 00:26:25 +0800

    prepare the load_data method

--- src/python/wswtm.py
+++ src/python/wswtm.py
@@ -11,6 +11,16 @@ from log import debug
 
 class wswtm():
     """
+        loads given images into a numpy array of shape (N, ch, r, c)
+        where 
+         - N is the number of images,
+         - ch is the number of the channels (3 for RGB, 4 for RGBA, etc.)
+         - is the number of the rows (height in pixels)
+         - is the number of the columns (width in pixels)
+    """
+    def load_data():
+        pass
+    """
         returns an array of possible tags for a give image (specified by a path)
     """
     def image2tags(self, path, treshold=0.75):
