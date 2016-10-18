9d5716bccfc5ac23477b4a17552f84c2f7c19e6a
Tomas Libal <tomas@libal.eu>
2016-10-18 22:37:51 +0800

    add wswtm class

---
+++ src/python/wswtm.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+
+np.random.seed(42)  # set deterministic reproducibility
+
+import image
+from log import debug
+
+class wswtm():
+    """
+        returns an array of possible tags for a give image (specified by a path)
+    """
+    def image2tags(path, treshold=0.75):
+        pass
+
+debug("wswtm init...") 
+
