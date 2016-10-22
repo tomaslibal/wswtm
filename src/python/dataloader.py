db93cf6f614480ce7ad3579a6a4b82c0a6ac5f5c
Tomas Libal <tomas@libal.eu>
2016-10-22 20:25:24 +0800

    add a dataloader class which will be responsible for loading the pixel data together with given classes for each pixel array

---
+++ src/python/dataloader.py
@@ -0,0 +1,5 @@
+class dataloader():
+    def __init__(self, path):
+        self.path = path
+
+    
---
+++ test/python/test_dataloader.py
@@ -0,0 +1,27 @@
+import unittest
+import os.path
+from os import remove
+
+from src.python.dataloader import dataloader
+
+class DataloaderTest(unittest.TestCase):
+    dummy_file = 'test/resources/dummy.txt'
+
+    @classmethod
+    def clean(self):
+        try:
+            remove(self.dummy_file)
+        except OSError:
+            pass
+
+    def setUp(self):
+        self.clean()
+        with open(self.dummy_file, 'w') as dummy:
+            dummy.write('abc')
+
+    @classmethod
+    def tearDownClass(self):
+        self.clean()
+
+    def test_loads_all_lines_even_if_last_line_does_not_have_line_break(self):
+        
