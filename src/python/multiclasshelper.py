f3186dd7e55d33b3b1b6a805c731bf2a02563328
Tomas Libal <tomas@libal.eu>
2016-10-23 18:20:43 +0800

    implement array_to_classes
    this takes an array of 1s and 0s and a dictionary of labels and their indexes and produces an array of labels corresponding to the indexes in the array

--- src/python/multiclasshelper.py
+++ src/python/multiclasshelper.py
@@ -1,6 +1,8 @@
+from collections import OrderedDict
+
 class MultiClassHelper():
     def get_class_dict(self, classes):
-       dct = {}
+       dct = OrderedDict()
        idx = 0
        for cls in classes:
            if not cls in dct:
@@ -15,4 +17,14 @@ class MultiClassHelper():
                 idx = dct[cls]
                 a[idx] = 1
         return a
+
+    def array_to_classes(self, a, dct):
+        labels = []
+        keys = dct.keys()
+        i = 0
+        for idx in a:
+            if idx is 1:
+                labels.append(keys[i])
+            i += 1
+        return labels
 
--- test/python/test_multiclasshelper.py
+++ test/python/test_multiclasshelper.py
@@ -1,6 +1,8 @@
 import unittest
 import numpy as np
 
+from collections import OrderedDict
+
 
 from src.python.multiclasshelper import MultiClassHelper
 
@@ -20,4 +22,10 @@ class ModelTrainerTest(unittest.TestCase):
         classes = [ 'foo', 'baz' ]
         a = self.mch.classes_to_array(classes, dct)
         self.assertEqual(True, np.array_equal([1, 0, 1], a))
+
+    def test_it_should_translate_an_array_back_to_its_respective_labels(self):
+        dct = OrderedDict([ ('foo', 0), ('bar', 1), ('baz', 2)])
+        a = [0, 1, 1]
+        labels = self.mch.array_to_classes(a, dct)
+        self.assertTrue(np.array_equal(['bar', 'baz'], labels))
 
