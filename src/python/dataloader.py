e6e46ab233bf7548ebd23804cafc439cadf61fdf
Tomas Libal <tomas@libal.eu>
2016-10-23 12:53:10 +0800

    test loading csv loads also the last line if it does not have a new line character

--- src/python/dataloader.py
+++ src/python/dataloader.py
@@ -1,8 +1,17 @@
+import numpy as np
+
 class dataloader():
     def __init__(self, path):
         self.path = path
 
-    def load_data(self):
-        x = []
-        y = []
-        return x, y
+    def load_data(self, num_cols=784, y_enable=True):
+        # assert isinstance(self.path, str)
+        # assert len(self.path) > 0
+        X = []
+        Y = []
+	data = np.loadtxt(self.path, delimiter=",", usecols=range(num_cols))
+        X = np.array(data[:,[0,num_cols-1]])
+        if y_enable is True:
+            Y = np.array(data[:,-num_cols])
+    
+        return X, Y
--- test/python/test_dataloader.py
+++ test/python/test_dataloader.py
@@ -17,7 +17,7 @@ class DataloaderTest(unittest.TestCase):
     def setUp(self):
         self.clean()
         with open(self.dummy_file, 'w') as dummy:
-            dummy.write('abc')
+            dummy.write('1,0,1,0\n0.5,0,0.5,1')
 
         self.dl = dataloader(self.dummy_file)
 
@@ -26,6 +26,6 @@ class DataloaderTest(unittest.TestCase):
         self.clean()
 
     def test_loads_all_lines_even_if_last_line_does_not_have_line_break(self):
-        x, y = self.dl.load_data()
-        self.assertEqual(1, len(x))
-        self.assertEqual(1, len(y))
+        x, y = self.dl.load_data(4, True)
+        self.assertEqual(2, len(x))
+        self.assertEqual(2, len(y))
