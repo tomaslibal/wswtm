9b338ae6f48f0f0e1cca4c343adba7bf826c2a41
Tomas Libal <tomas@libal.eu>
2016-10-23 12:56:18 +0800

    test that the loader does not add an empty line if the last character is a new line character

--- test/python/test_dataloader.py
+++ test/python/test_dataloader.py
@@ -14,16 +14,27 @@ class DataloaderTest(unittest.TestCase):
         except OSError:
             pass
 
+    @classmethod
+    def write_to_dummy(self, string):
+        with open(self.dummy_file, 'w') as dummy:
+            dummy.write(string)
+
     def setUp(self):
         self.clean()
-        with open(self.dummy_file, 'w') as dummy:
-            dummy.write('1,0,1,0\n0.5,0,0.5,1')
+        self.write_to_dummy('1,0,1,0\n0.5,0,0.5,1')
 
         self.dl = dataloader(self.dummy_file)
 
     @classmethod
     def tearDownClass(self):
         self.clean()
+
+    def test_loads_all_lines(self):
+        self.clean()
+        self.write_to_dummy('1,2,3\n4,5,6\n7,8,9\n')
+        x, y = self.dl.load_data(3, True)
+        self.assertEqual(3, len(x))
+        self.assertEqual(3, len(y))
 
     def test_loads_all_lines_even_if_last_line_does_not_have_line_break(self):
         x, y = self.dl.load_data(4, True)
