9d425a2a6e4d4b0f2c2e7e76b03459cdfe5c2496
Tomas Libal <tomas@libal.eu>
2016-11-02 22:18:16 +0800

    add __init__ and implement the load_model methods

--- src/python/wswtm.py
+++ src/python/wswtm.py
@@ -17,12 +17,37 @@ class wswtm():
     green = False
     blue = False
 
+    default_model_path = 'resources/models/'
+    default_model_name = 'basic_cnn'
+
+    load_on_init = True
+
+    def __init__(self):
+        if load_on_init is True:
+            self.init()
+
+    def init(self):
+        model, dct = self.load_model(self.default_model_path, self.default_model_name)
+
+        self.model = model
+        self.dct = dct
 
     """
         Loads a trained model
     """
-    def load_model(self, model):
-        self.model = model
+    def load_model(self, model_path, model_name):
+        self.model_path = model_path
+        self.model_name = model_name
+
+        json_file = open(self.model_path + self.model_name + '.json', 'r')
+        loaded_model = json_file.read()
+        json_file.close()
+        
+        model = model_from_json(loaded_model)
+        model.load_weights(self.model_path + self.model_name + ".h5")
+        dct = pickle.load(open(self.model_path + self.model_name + '_dct.p', 'rb'))
+
+        return model, dct
 
     """
         returns an array of possible tags for a give image (specified by a path)
