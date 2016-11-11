b72ca822c7d7a0fbbc92530210a3a57367e79065
Tomas Libal <tomas@libal.eu>
2016-11-11 22:50:17 +0800

    made some tweaks to the basic cnn model
    now uses the kernel_size for the kernel_size in the conv layers

--- src/python/models/basic_cnn.py
+++ src/python/models/basic_cnn.py
@@ -7,22 +7,22 @@ class basic_cnn():
         self.name = "basic cnn model"
 
         input_shape = shape
-        nb_filters = 32
+        nb_filters = 16
         kernel_size = 3
 
         self.feature_layers = [
     		Convolution2D(nb_filters, kernel_size, kernel_size, input_shape=input_shape, border_mode='valid', activation='relu'),
 		Dropout(0.2),
-    		Convolution2D(nb_filters, 1, kernel_size, activation='relu'),
-	        MaxPooling2D(pool_size=(1,2)),
-    	        Dropout(0.25),
-    	        Convolution2D(nb_filters*2, 1, 1, activation='relu', border_mode='same'),
+    		Convolution2D(nb_filters, kernel_size, kernel_size, activation='relu'),
+	        MaxPooling2D(pool_size=(2,2)),
+    	        # Dropout(0.25),
+    	        Convolution2D(nb_filters*2, kernel_size, kernel_size, activation='relu', border_mode='same'),
                 Flatten(),
 	]
 
 	self.classification_layers = [
     		Dense(64, activation='relu'),
-		Dropout(0.5),
+		# Dropout(0.5),
     		Dense(64, activation='relu'),
 	        Dropout(0.5),    
                 Dense(self.number_classes),
@@ -38,7 +38,7 @@ class basic_cnn():
         if (self.number_classes > 2):
             loss_fcn = 'categorical_crossentropy'
         model.compile(loss=loss_fcn,
-                  optimizer='adadelta',
+                  optimizer='adam',
                   metrics=['accuracy'])
 
     def fit(self, model, X_train, Y_train, X_test, Y_test, batch_size=256, nb_epoch=32, verbose=1):
